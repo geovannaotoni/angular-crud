74160e9aeb96aabcc99148cc310379b60f48bcd4
Geovanna Otoni <geovannaotoni@gmail.com>
2024-08-18 15:01:00 -0300

    feat: create funcionario service

---
+++ src/app/services/funcionario.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Funcionario } from '../models/Funcionarios';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FuncionarioService {
+  private apiUrl = 'http://localhost:3000/funcionarios';
+
+  constructor(private http: HttpClient) { }
+
+  getFuncionarios(): Observable<Funcionario[]> {
+    return this.http.get<Funcionario[]>(this.apiUrl);
+  }
+
+  getFuncionarioById(id: number): Observable<Funcionario> {
+    return this.http.get<Funcionario>(`${this.apiUrl}/${id}`);
+  }
+
+  addFuncionario(funcionario: Funcionario): Observable<Funcionario> {
+    return this.http.post<Funcionario>(this.apiUrl, funcionario);
+  }
+
+  updateFuncionario(funcionario: Funcionario): Observable<Funcionario> {
+    return this.http.put<Funcionario>(`${this.apiUrl}/${funcionario.id}`, funcionario);
+  }
+
+  deleteFuncionario(id: number): Observable<Funcionario> {
+    return this.http.delete<Funcionario>(`${this.apiUrl}/${id}`);
+  }
+}
